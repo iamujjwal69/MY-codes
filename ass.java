871d5ebf916feb21f8c7ae6f723b1317b6b17b4c
iamujjwal69 <ujjwalsharma2353@gmail.com>
2025-03-07 00:31:29 +0530

    This is My First Java Code

---
+++ ass.java
@@ -0,0 +1,7 @@
+class Ass{
+    public static void main(String[] args) {
+        System.out.println("I like the Pizza!");
+        System.out.println("It's Really Good!");
+        System.out.println("Please Buy me a Pizza!");
+    }
+}
